a1dc2b2d1dce32ba2d915f0d22bf75816377fa72
Justin Edelson <jedelson@adobe.com>
2019-10-17 18:29:07 -0400

    add delete-pipeline command. fixes #41

---
+++ test/commands/delete-pipeline.test.js
@@ -0,0 +1,74 @@
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const { cli } = require('cli-ux')
+const { setStore } = require('@adobe/aio-cna-core-config')
+const DeletePipelineCommand = require('../../src/commands/cloudmanager/delete-pipeline')
+
+beforeEach(() => {
+    setStore({})
+})
+
+test('delete-pipeline - missing arg', async () => {
+    expect.assertions(2)
+
+    let runResult = DeletePipelineCommand.run([])
+    await expect(runResult instanceof Promise).toBeTruthy()
+    await expect(runResult).rejects.toSatisfy(err => err.message.indexOf("Missing 1 required arg") === 0)
+})
+
+test('delete-pipeline - missing config', async () => {
+    expect.assertions(3)
+
+    let runResult = DeletePipelineCommand.run(["--programId", "5", "10"])
+    await expect(runResult instanceof Promise).toBeTruthy()
+    await expect(runResult).resolves.toEqual(undefined)
+    await expect(cli.action.stop.mock.calls[0][0]).toBe("missing config data: jwt-auth")
+})
+
+test('delete-pipeline - bad pipeline', async () => {
+    setStore({
+        'jwt-auth': JSON.stringify({
+            client_id: '1234',
+            jwt_payload: {
+                iss: "good"
+            }
+        }),
+    })
+
+    expect.assertions(3)
+
+    let runResult = DeletePipelineCommand.run(["--programId", "5", "10"])
+    await expect(runResult instanceof Promise).toBeTruthy()
+    await expect(runResult).resolves.toEqual(undefined)
+    await expect(cli.action.stop.mock.calls[0][0]).toBe("Cannot delete pipeline. Pipeline 10 does not exist.")
+})
+
+test('delete-pipeline - success', async () => {
+    setStore({
+        'jwt-auth': JSON.stringify({
+            client_id: '1234',
+            jwt_payload: {
+                iss: "good"
+            }
+        }),
+    })
+
+    expect.assertions(3)
+
+    let runResult = DeletePipelineCommand.run(["--programId", "5", "5"])
+    await expect(runResult instanceof Promise).toBeTruthy()
+    await expect(runResult).resolves.toEqual({})
+    await expect(cli.action.stop.mock.calls[0][0]).toBe("deleted pipeline ID 5")
+})
+
+
