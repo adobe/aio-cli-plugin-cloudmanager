e0a06311c8f156bec91485396b8e44767634218b
Justin Edelson <jedelson@adobe.com>
2020-04-27 14:12:54 -0400

    hide base environment variables command from README. fixes #101 (#102)

--- src/commands/cloudmanager/list-environment-variables.js
+++ src/commands/cloudmanager/list-environment-variables.js
@@ -10,7 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const BaseEnvironmentVariablesCommand = require('./base-environment-variables-command')
+const BaseEnvironmentVariablesCommand = require('../../base-environment-variables-command')
 const { getProgramId } = require('../../cloudmanager-helpers')
 const commonFlags = require('../../common-flags')
 
