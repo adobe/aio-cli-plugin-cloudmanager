694523dc0f3e8d50510f15901c13caf4930c7a89
Justin Edelson <jedelson@adobe.com>
2021-03-05 11:02:49 -0500

    feat(config): add hook for validation of the IMS context configuration. fixes #233 and #216

---
+++ src/hooks/prerun/check-ims-context-config.js
@@ -0,0 +1,45 @@
+/*
+Copyright 2021 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const Config = require('@adobe/aio-lib-core-config')
+const { defaultContextName } = require('../../cloudmanager-helpers')
+
+const configKey = `ims.contexts.${defaultContextName}`
+
+const requiredKeys = ['client_id', 'client_secret', 'technical_account_id', 'meta_scopes', 'ims_org_id', 'private_key']
+
+const requiredMetaScope = 'ent_cloudmgr_sdk'
+
+module.exports = function () {
+  const config = Config.get(configKey)
+
+  if (!config) {
+    // in the future we might have a command that doesn't require configuration in which case this should change
+    this.error(`There is no IMS context configuration defined for ${configKey}.`)
+  }
+
+  const missingKeys = []
+
+  requiredKeys.forEach(key => {
+    if (!config[key]) {
+      missingKeys.push(key)
+    }
+  })
+
+  if (missingKeys.length > 0) {
+    this.error(`One or more of the required fields in ${configKey} were not set. Missing keys were ${missingKeys.join(', ')}.`)
+  }
+
+  const metaScopes = config.meta_scopes
+  if (!metaScopes.includes || !metaScopes.includes(requiredMetaScope)) {
+    this.error(`The configuration ${configKey} is missing the required metascope ${requiredMetaScope}.`)
+  }
+}
