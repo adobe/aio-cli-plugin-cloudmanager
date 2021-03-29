212bf9deb98564f00c528fcfe3009e4e3f5c43a4
Justin Edelson <jedelson@adobe.com>
2021-03-29 16:41:07 -0400

    chore(deps): add some environment tests

---
+++ test/commands/environment/list-variables.test.js
@@ -0,0 +1,92 @@
+/*
+Copyright 2019 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const { cli } = require('cli-ux')
+const { init, mockSdk } = require('@adobe/aio-lib-cloudmanager')
+const { resetCurrentOrgId, setCurrentOrgId } = require('@adobe/aio-lib-ims')
+const { setStore } = require('@adobe/aio-lib-core-config')
+const ListEnvironmentVariablesCommand = require('../../../src/commands/cloudmanager/environment/list-variables')
+
+beforeEach(() => {
+  resetCurrentOrgId()
+})
+
+test('list-environment-variables - missing arg', async () => {
+  expect.assertions(2)
+
+  const runResult = ListEnvironmentVariablesCommand.run([])
+  await expect(runResult instanceof Promise).toBeTruthy()
+  await expect(runResult).rejects.toSatisfy(err => err.message.indexOf('Missing 1 required arg') > -1)
+})
+
+test('list-environment-variables - missing programId', async () => {
+  expect.assertions(2)
+
+  const runResult = ListEnvironmentVariablesCommand.run(['1'])
+  await expect(runResult instanceof Promise).toBeTruthy()
+  await expect(runResult).rejects.toSatisfy(err => err.message.indexOf('Program ID must be specified either as --programId flag or through cloudmanager_programid') === 0)
+})
+
+test('list-environment-variables - missing config', async () => {
+  expect.assertions(2)
+
+  const runResult = ListEnvironmentVariablesCommand.run(['1', '--programId', '5'])
+  await expect(runResult instanceof Promise).toBeTruthy()
+  await expect(runResult).rejects.toEqual(new Error('Unable to find IMS context aio-cli-plugin-cloudmanager'))
+})
+
+test('list-environment-variables - success', async () => {
+  setCurrentOrgId('good')
+  setStore({
+    cloudmanager_programid: '4',
+  })
+
+  expect.assertions(7)
+
+  const runResult = ListEnvironmentVariablesCommand.run(['1'])
+  await expect(runResult instanceof Promise).toBeTruthy()
+
+  await runResult
+  await expect(init.mock.calls.length).toEqual(1)
+  await expect(init).toHaveBeenCalledWith('good', 'test-client-id', 'fake-token', 'https://cloudmanager.adobe.io')
+  await expect(mockSdk.getEnvironmentVariables.mock.calls.length).toEqual(1)
+  await expect(mockSdk.getEnvironmentVariables).toHaveBeenCalledWith('4', '1')
+  await expect(cli.table.mock.calls[0][1].value.get({
+    name: 'I_AM_A_SECRET',
+    type: 'secretString',
+  })).toBe('****')
+  await expect(cli.table.mock.calls[0][1].value.get({
+    name: 'KEY',
+    type: 'string',
+    value: 'value',
+  })).toBe('value')
+})
+
+test('list-environment-variables for "e" prefixed env id - success', async () => {
+  setCurrentOrgId('good')
+  setStore({
+    cloudmanager_programid: '4',
+  })
+
+  expect.assertions(2)
+
+  const runResult = ListEnvironmentVariablesCommand.run(['e1'])
+  await expect(runResult instanceof Promise).toBeTruthy()
+  await expect(runResult).resolves.toMatchObject([{
+    name: 'KEY',
+    type: 'string',
+    value: 'value',
+  }, {
+    name: 'I_AM_A_SECRET',
+    type: 'secretString',
+  }])
+})
---
+++ test/commands/environment/unbind-ip-allowlist.test.js
@@ -0,0 +1,66 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const UnunbindIpAllowlist = require('../../../src/commands/cloudmanager/environment/unbind-ip-allowlist')
+
+const { cli } = require('cli-ux')
+const { setCurrentOrgId } = require('@adobe/aio-lib-ims')
+const { generateNewMock } = require('@adobe/aio-lib-cloudmanager')
+
+let mockSdk
+
+beforeEach(() => {
+  setCurrentOrgId('good')
+  mockSdk = generateNewMock()
+})
+
+test('unbind -- bad service', async () => {
+  mockSdk.removeIpAllowlistBinding = jest.fn(() => { throw new Error('test error') }) // should never get called
+
+  expect.assertions(3)
+
+  const runResult = UnunbindIpAllowlist.run(['--programId', '1', '2', '3', 'NONE'])
+  await expect(runResult instanceof Promise).toBeTruthy()
+
+  await expect(runResult).rejects.toThrow(/^Expected NONE to be one of: author, publish/)
+  await expect(mockSdk.removeIpAllowlistBinding.mock.calls.length).toEqual(0)
+})
+
+test('unbind -- fail', async () => {
+  mockSdk.removeIpAllowlistBinding = jest.fn(() => { throw new Error('test error') })
+
+  expect.assertions(4)
+
+  const runResult = UnunbindIpAllowlist.run(['--programId', '1', '2', '3', 'author'])
+  await expect(runResult instanceof Promise).toBeTruthy()
+
+  await expect(runResult).rejects.toThrow('test error')
+  await expect(mockSdk.removeIpAllowlistBinding.mock.calls.length).toEqual(1)
+  await expect(mockSdk.removeIpAllowlistBinding).toHaveBeenCalledWith('1', '3', '2', 'author')
+})
+
+test('unbind -- success', async () => {
+  mockSdk.removeIpAllowlistBinding = jest.fn(() => Promise.resolve({
+    id: '5',
+  }))
+
+  expect.assertions(6)
+
+  const runResult = UnunbindIpAllowlist.run(['--programId', '1', '2', '3', 'author'])
+  await expect(runResult instanceof Promise).toBeTruthy()
+
+  await expect(runResult).resolves.toBeTruthy()
+  await expect(mockSdk.removeIpAllowlistBinding.mock.calls.length).toEqual(1)
+  await expect(mockSdk.removeIpAllowlistBinding).toHaveBeenCalledWith('1', '3', '2', 'author')
+  await expect(cli.action.start.mock.calls[0][0]).toEqual('removing IP allowlist 3 binding from environment 2 (author)')
+  await expect(cli.action.stop.mock.calls[0][0]).toEqual('removed')
+})
