0e9abe4e65ebe050a31173a8faa8fa29a623d79d
David Krause <dkrause@adobe.com>
2021-08-27 11:39:52 -0500

    feat(commerce): add indexer:reindex fixes #455

---
+++ src/commands/cloudmanager/commerce/bin-magento/indexer/reindex.js
@@ -0,0 +1,49 @@
+/*
+Copyright 2021 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const BaseCommerceCliCommand = require('../../../../../base-commerce-cli-command')
+const { getProgramId } = require('../../../../../cloudmanager-helpers')
+const commonFlags = require('../../../../../common-flags')
+
+class IndexerReindexCommand extends BaseCommerceCliCommand {
+  async run () {
+    const { args, flags } = this.parse(IndexerReindexCommand)
+
+    const programId = getProgramId(flags)
+
+    const result = await this.runSync(programId, args.environmentId,
+      {
+        type: 'bin/magento',
+        command: 'indexer:reindex',
+      },
+      1000, 'indexer:reindex')
+
+    return result
+  }
+}
+
+IndexerReindexCommand.description = 'commerce indexer reindex'
+
+IndexerReindexCommand.flags = {
+  ...commonFlags.global,
+  ...commonFlags.programId,
+}
+
+IndexerReindexCommand.args = [
+  { name: 'environmentId', required: true, description: 'the environment id' },
+]
+
+IndexerReindexCommand.aliases = [
+  'cloudmanager:commerce:indexer-reindex',
+]
+
+module.exports = IndexerReindexCommand
