596f96bc282dcde38ea61078b9949109971795c4
David Krause <dkrause@adobe.com>
2021-08-30 14:10:52 -0500

    feat(commerce): add cache:flush fixes #462

---
+++ src/commands/cloudmanager/commerce/bin-magento/cache/flush.js
@@ -0,0 +1,50 @@
+/*
+Copyright 2021 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const BaseCommerceCliCommand = require('../../../../../base-commerce-cli-command')
+const { getProgramId } = require('../../../../../cloudmanager-helpers')
+const commonFlags = require('../../../../../common-flags')
+const commonArgs = require('../../../../../common-args')
+
+class CacheFlushCommand extends BaseCommerceCliCommand {
+  async run () {
+    const { args, flags } = this.parse(CacheFlushCommand)
+
+    const programId = getProgramId(flags)
+
+    const result = await this.runSync(programId, args.environmentId,
+      {
+        type: 'bin/magento',
+        command: 'cache:flush',
+      },
+      1000, 'cache:flush')
+
+    return result
+  }
+}
+
+CacheFlushCommand.description = 'commerce cache flush'
+
+CacheFlushCommand.flags = {
+  ...commonFlags.global,
+  ...commonFlags.programId,
+}
+
+CacheFlushCommand.args = [
+  commonArgs.environmentId,
+]
+
+CacheFlushCommand.aliases = [
+  'cloudmanager:commerce:cache-flush',
+]
+
+module.exports = CacheFlushCommand
