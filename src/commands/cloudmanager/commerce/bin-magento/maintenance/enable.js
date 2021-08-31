253446382c5dbb4586662aeba5d46e1d515f7e16
David Krause <dkrause@adobe.com>
2021-08-31 06:28:27 -0500

    feat(commerce): add maintenance:enable fixes #464 (#465)

---
+++ src/commands/cloudmanager/commerce/bin-magento/maintenance/enable.js
@@ -0,0 +1,50 @@
+/*
+Copyright 2021 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const BaseCommerceCliCommand = require('../../../../../base-commerce-cli-command')
+const { getProgramId } = require('../../../../../cloudmanager-helpers')
+const commonFlags = require('../../../../../common-flags')
+const commonArgs = require('../../../../../common-args')
+
+class MaintenanceEnableCommand extends BaseCommerceCliCommand {
+  async run () {
+    const { args, flags } = this.parse(MaintenanceEnableCommand)
+
+    const programId = getProgramId(flags)
+
+    const result = await this.runSync(programId, args.environmentId,
+      {
+        type: 'bin/magento',
+        command: 'maintenance:enable',
+      },
+      1000, 'maintenance:enable')
+
+    return result
+  }
+}
+
+MaintenanceEnableCommand.description = 'commerce maintenance enable'
+
+MaintenanceEnableCommand.flags = {
+  ...commonFlags.global,
+  ...commonFlags.programId,
+}
+
+MaintenanceEnableCommand.args = [
+  commonArgs.environmentId,
+]
+
+MaintenanceEnableCommand.aliases = [
+  'cloudmanager:commerce:maintenance-enable',
+]
+
+module.exports = MaintenanceEnableCommand
